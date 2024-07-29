2a78141eb7f874d623d6b9d046f662310570efb1
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-07-29 17:41:39 +0700

    Author Partial Update Endpoint

--- src/main/java/com/devtiro/database/controllers/AuthorController.java
+++ src/main/java/com/devtiro/database/controllers/AuthorController.java
@@ -13,6 +13,7 @@ import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -69,6 +70,21 @@ public class AuthorController {
         AuthorEntity savedAuthorEntity = authorService.save(authorEntity);
         return new ResponseEntity<>(
                 authorMapper.mapTo(savedAuthorEntity),
+                HttpStatus.OK);
+    }
+
+    @PatchMapping(path = "/authors/{id}")
+    public ResponseEntity<AuthorDto> partialUpdate(
+            @PathVariable("id") Long id,
+            @RequestBody AuthorDto authorDto) {
+        if (!authorService.isExists(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
+        AuthorEntity updatedAuthor = authorService.partialUpdate(id, authorEntity);
+        return new ResponseEntity<>(
+                authorMapper.mapTo(updatedAuthor),
                 HttpStatus.OK);
     }
 }
